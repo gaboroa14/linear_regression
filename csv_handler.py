11f297bd80642d20bb3d394fa2d2b1596ce27cf3
Anyi <anyeliv02@gmail.com>
2020-07-04 19:04:54 -0400

    modas y medianas

--- csv_handler.py
+++ csv_handler.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 import statistics as esta
+from scipy import stats as estasc
 
 def generar_diccionario():
     with open('datos1.csv') as csv_file:
@@ -60,6 +61,71 @@ def Medias(dic):
             edades.append(fila['edad'])
         print(elemento)
         print(esta.mean(edades))
-    #
 
-Medias(generar_diccionario())
+dic = generar_diccionario()
+
+#Medias(dic)
+
+#def Medianas(dic):
+    #Calcular las medianas de las edades 
+
+def Modas(dic):
+    for elemento in dic:
+        categoria = (dic[elemento])
+        edades = list()
+        for fila in categoria:
+            edades.append(fila['edad'])
+        moda = "La moda de la edad de la categoría {} es el valor {} con {} apariciones."
+        print(moda.format(elemento, str(estasc.mode(edades)[0][0]),str(estasc.mode(edades)[1][0])))
+    for elemento in dic:
+        categoria = (dic[elemento])
+        edades = list()
+        for fila in categoria:
+            edades.append(fila['imc'])
+        moda = "La moda de la imc de la categoría {} es el valor {} con {} apariciones."
+        print(moda.format(elemento, str(estasc.mode(edades)[0][0]),str(estasc.mode(edades)[1][0])))
+    for elemento in dic:
+        categoria = (dic[elemento])
+        edades = list()
+        for fila in categoria:
+            edades.append(fila['hijos'])
+        moda = "La moda de los hijos de la categoría {} es el valor {} con {} apariciones."
+        print(moda.format(elemento, str(estasc.mode(edades)[0][0]),str(estasc.mode(edades)[1][0])))
+    for elemento in dic:
+        categoria = (dic[elemento])
+        edades = list()
+        for fila in categoria:
+            edades.append(fila['cargos'])
+        moda = "La moda de los cargos de la categoría {} es el valor {} con {} apariciones."
+        print(moda.format(elemento, str(estasc.mode(edades)[0][0]),str(estasc.mode(edades)[1][0])))
+
+def Medianas(dic):
+    for elemento in dic:
+        categoria = (dic[elemento])
+        edades = list()
+        for fila in categoria:
+            edades.append(fila['edad'])
+        mediana = "La mediana de la edad de la categoría {} es el valor {}."
+        print(mediana.format(elemento, str(esta.median(edades))))
+    for elemento in dic:
+        categoria = (dic[elemento])
+        edades = list()
+        for fila in categoria:
+            edades.append(fila['imc'])
+        mediana = "La mediana de la imc de la categoría {} es el valor {}."
+        print(mediana.format(elemento, str(esta.median(edades))))
+    for elemento in dic:
+        categoria = (dic[elemento])
+        edades = list()
+        for fila in categoria:
+            edades.append(fila['hijos'])
+        mediana = "La mediana de los hijos de la categoría {} es el valor {}."
+        print(mediana.format(elemento, str(esta.median(edades))))
+    for elemento in dic:
+        categoria = (dic[elemento])
+        edades = list()
+        for fila in categoria:
+            edades.append(fila['cargos'])
+        mediana = "La mediana de los cargos de la categoría {} es el valor {}."
+        print(mediana.format(elemento, str(esta.median(edades))))
+Medianas(dic)
