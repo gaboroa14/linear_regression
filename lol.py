e7cc88e7caa24d1a896b28284fe83b3e0fc659dd
Gabriel Roa <gaboroab14@gmail.com>
2020-07-05 11:38:03 -0400

    modelolineal

--- lol.py
+++ lol.py
@@ -1,14 +1,38 @@
 import csv
 
-with open('datos1.csv') as csv_file:
+with open('datos2.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     datos = list(csv_reader)
     nuevos_datos = list()
+    datos[0].append('ne')
+    datos[0].append('nw')
+    datos[0].append('se')
+    datos[0].append('sw')
+    print(datos[0])
     for row in datos[0:]:
-        row[6] = row[6].replace('.','')
-        print(row)
+        if row[5] == '0':
+            row.append('1')
+            row.append('0')
+            row.append('0')
+            row.append('0')
+        elif row[5] == '1':
+            row.append('0')
+            row.append('1')
+            row.append('0')
+            row.append('0')
+        elif row[5] == '2':
+            row.append('0')
+            row.append('0')
+            row.append('1')
+            row.append('0')
+        else:
+            row.append('0')
+            row.append('0')
+            row.append('0')
+            row.append('1')
         nuevos_datos.append(row)
-    result_file = open("nombres.csv",'w')
+        print(row)
+    result_file = open("datos3.csv",'w')
     wr = csv.writer(result_file, dialect='excel')
     for elemento in nuevos_datos:
         wr.writerow(elemento)
